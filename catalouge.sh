53b3fa05b0e3dcafc1b6e6c25ef6562340546f38
Sudheer <info.sudheer30@gmail.com>
2023-08-23 14:16:19 +0530

    initial commit

---
+++ catalouge.sh
@@ -0,0 +1,103 @@
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+USERID=$(id -u)
+USER_ROBOSHOP=$(id roboshop)
+VALIDATE_APP_DIR=$(cd /app)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+ echo -e " Execute with root user"
+ exit 1
+ fi
+
+ VALIDATE(){
+if [ $1 -ne 0 ];
+then
+    echo -e "$2....$R FAILURE $N "
+    exit 1
+else
+   echo -e "$2... $G SUCCESS $N "
+   fi
+ 
+ }
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM Source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing NodeJS"
+
+#once the user is created, if you run this script 2nd time
+# this command will defnitely fail
+# IMPROVEMENT: first check the user already exist or not, if not exist then create
+if [ $USER_ROBOSHOP -ne 0 ];
+then 
+    echo -e "$Y USER roboshop is not present so creating one now..$N"
+    useradd roboshop &>>$LOGFILE
+ else 
+     echo -e "$Y USER roboshop is already present so not skipping now.$N"
+ 
+ fi
+
+#write a condition to check directory already exist or not
+if [ $VALIDATE_APP_DIR -ne 0 ];
+then 
+    echo -e " $Y /app directory not there so creating one $N"
+    mkdir /app &>>$LOGFILE
+else
+    echo -e "$Y /app directory present so not skipping now.$N"
+
+fi
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+
+VALIDATE $? "downloading catalogue artifact"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving into app directory"
+
+unzip /tmp/catalogue.zip &>>$LOGFILE
+
+VALIDATE $? "unzipping catalogue"
+
+npm install &>>$LOGFILE
+
+VALIDATE $? "Installing dependencies"
+
+# give full path of catalogue.service because we are inside /app
+cp /root/roboshop_shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+
+VALIDATE $? "copying catalogue.service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon reload"
+
+systemctl enable catalogue &>>$LOGFILE
+
+VALIDATE $? "Enabling Catalogue"
+
+systemctl start catalogue &>>$LOGFILE
+
+VALIDATE $? "Starting Catalogue"
+
+cp /root/roboshop_shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+
+VALIDATE $? "Copying mongo repo"
+
+yum install mongodb-org-shell -y &>>$LOGFILE
+
+VALIDATE $? "Installing mongo client"
+
+mongo --host mongodb.sudheerdevops.online </app/schema/catalogue.js &>>$LOGFILE
+
+VALIDATE $? "loading catalogue data into mongodb"
