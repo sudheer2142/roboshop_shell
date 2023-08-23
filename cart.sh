f23f2e210b624f2c9e4fd7809771a61d96e15ac7
Sudheer <info.sudheer30@gmail.com>
2023-08-23 15:44:41 +0530

    code updated

---
+++ cart.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM Source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing NodeJS"
+
+#once the user is created, if you run this script 2nd time
+# this command will defnitely fail
+# IMPROVEMENT: first check the user already exist or not, if not exist then create
+USER_ROBOSHOP=$(id roboshop)
+if [ $? -ne 0 ];
+then 
+    echo -e "$Y...USER roboshop is not present so creating one now..$N"
+    useradd roboshop &>>$LOGFILE
+else 
+    echo -e "$G...USER roboshop is already present so  skipping now.$N"
+ fi
+
+#write a condition to check directory already exist or not
+VALIDATE_APP_DIR=$(cd /app)
+#write a condition to check directory already exist or not
+if [ $? -ne 0 ];
+then 
+    echo -e " $Y /app directory not there so creating one $N"
+    mkdir /app &>>$LOGFILE   
+else
+    echo -e "$G /app directory already present so skipping ....$N" 
+    fi
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
+
+VALIDATE $? "downloading cart artifact"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving into app directory"
+
+unzip /tmp/cart.zip &>>$LOGFILE
+
+VALIDATE $? "unzipping cart"
+
+npm install &>>$LOGFILE
+
+VALIDATE $? "Installing dependencies"
+
+# give full path of cart.service because we are inside /app
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>$LOGFILE
+
+VALIDATE $? "copying cart.service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon reload"
+
+systemctl enable cart &>>$LOGFILE
+
+VALIDATE $? "Enabling cart"
+
+systemctl start cart &>>$LOGFILE
+
+VALIDATE $? "Starting cart"
